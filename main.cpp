a9534047cf379773f467e26feea3d104738f91e7
cherifi rabah <rabouh9131@yahoo.com>
2017-04-19 13:52:23 +0200

    first commit

--- main.cpp
+++ main.cpp
@@ -120,7 +120,13 @@ std::complex<long double> c(r, i);// constante c
           image.at<Vec3b>(x,y)[1]=0; 
           image.at<Vec3b>(x,y)[2]=0;
         }
-       =r; 
+        else{//sinon on  est pas dans la suite et on met de la couleur selon le nombre d'itération 
+          unsigned char couleur=(int)((float)iteration/(float)ITERATIONMAX*360);
+          unsigned char r,g,b;
+	  hsvtorgb( &r,&g,&b,couleur,150,150);//hsvtorgb(rouge,bleu,vert,teinte,saturation,brillance)
+	  image.at<Vec3b>(x,y)[0]=b;
+          image.at<Vec3b>(x,y)[1]=g;
+          image.at<Vec3b>(x,y)[2]=r; 
       }
     } 
   }
@@ -139,7 +145,36 @@ hsvtorgb(unsigned char *r, unsigned char *g, unsigned char *b, unsigned char h, 
 {
     unsigned char region, fpart, p, q, t;
     
- 
+    if(s == 0) {
+        /* color is grayscale */
+        *r = *g = *b = v;
+        return;
+    }
+    
+    /* make hue 0-5 */
+    region = h / 43;
+    /* find remainder part, make it from 0-255 */
+    fpart = (h - (region * 43)) * 6;
+    
+    /* calculate temp vars, doing integer multiplication */
+    p = (v * (255 - s)) >> 8;
+    q = (v * (255 - ((s * fpart) >> 8))) >> 8;
+    t = (v * (255 - ((s * (255 - fpart)) >> 8))) >> 8;
+        
+    /* assign temp vars based on color cone region */
+    switch(region) {
+        case 0:
+            *r = v; *g = t; *b = p; break;
+        case 1:
+            *r = q; *g = v; *b = p; break;
+        case 2:
+            *r = p; *g = v; *b = t; break;
+        case 3:
+            *r = p; *g = q; *b = v; break;
+        case 4:
+            *r = t; *g = p; *b = v; break;
+        default:
+            *r = v; *g = p; *b = q; break;
     }
     
     return;
